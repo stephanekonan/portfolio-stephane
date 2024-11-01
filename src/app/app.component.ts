18c7859be999cd87b68d3230e1d0f24fa51ee0b7
stephane_konan <stephanekonan.dev@gmail.com>
2024-11-01 01:13:34 +0000

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { FlowbiteService } from '../services/flowbite.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'stephane-dev-profil';
+
+  constructor(private flowbiteService: FlowbiteService) { }
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite(flowbite => {
+      console.log('Flowbite loaded', flowbite);
+    });
+  }
+
 }
