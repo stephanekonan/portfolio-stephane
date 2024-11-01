81e0f7b42fe935f36519330d66bccf60e21f1ba5
stephane_konan <stephanekonan.dev@gmail.com>
2024-11-01 01:45:39 +0000

    Copy email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,52 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { FlowbiteService } from '../services/flowbite.service';
+import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'stephane-dev-profil';
 
+  email = 'stephanekonan@gmail.com';
+  isCopied = false;
+
   constructor(private flowbiteService: FlowbiteService) { }
 
   ngOnInit(): void {
     this.flowbiteService.loadFlowbite(flowbite => {
       console.log('Flowbite loaded', flowbite);
+    });
+  }
+
+  copyEmail() {
+    navigator.clipboard.writeText(this.email).then(() => {
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'bottom-right',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      Toast.fire({
+        icon: 'success',
+        title: 'Email copié'
+      })
+      setTimeout(() => {
+        this.isCopied = false;
+      }, 3000);
+    }).catch(err => {
+      console.error('Erreur de copie : ', err);
     });
   }
 
