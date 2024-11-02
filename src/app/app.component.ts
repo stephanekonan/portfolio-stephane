da1c6473316b6382292c18f1f2682baafaef36ab
stephane_konan <stephanekonan.dev@gmail.com>
2024-11-02 10:06:41 +0000

    Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FlowbiteService } from '../services/flowbite.service';
 import { CommonModule } from '@angular/common';
@@ -12,10 +12,27 @@ import Swal from 'sweetalert2';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'stephane-dev-profil';
+  @ViewChild('header') header: ElementRef | undefined;
 
+  title = 'stephane-dev-profil';
   email = 'stephanekonan@gmail.com';
   isCopied = false;
+
+  images = [
+    { img: 'img/angular_gradient.png' },
+    { img: 'img/laravel.png' },
+    { img: 'img/tailwindcss.svg' },
+    { img: 'img/flowbite.png' },
+    { img: 'img/nestjs.svg' },
+    { img: 'img/kotlin.png' },
+    { img: 'img/firebase.png' },
+    { img: 'img/postgresql.png' },
+    { img: 'img/github.png' },
+    { img: 'img/git.png' },
+    { img: 'img/figma.svg' },
+    { img: 'img/ai.png' },
+    { img: 'img/ae.png' }
+  ];
 
   constructor(private flowbiteService: FlowbiteService) { }
 
@@ -23,6 +40,13 @@ export class AppComponent {
     this.flowbiteService.loadFlowbite(flowbite => {
       console.log('Flowbite loaded', flowbite);
     });
+  }
+
+  downloadCV() {
+    const link = document.createElement('a');
+    link.href = 'docs/cv_konan_koffi_louis_stephane_fullstack_dev.pdf';
+    link.download = 'cv_stephane_konan.pdf';
+    link.click();
   }
 
   copyEmail() {
@@ -49,5 +73,6 @@ export class AppComponent {
       console.error('Erreur de copie : ', err);
     });
   }
+
 
 }
